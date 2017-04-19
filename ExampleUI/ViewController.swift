f3207dbdc80f1b0403c4a3406dd08335a1916eab
Alexander Shoshiashvili <alex.shoshiashvili@mac51.hiq.ru>
2017-04-19 15:04:28 +0300

    Initial Commit

---
+++ ExampleUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleUI
+//
+//  Created by Alexander Shoshiashvili on 19/04/2017.
+//  Copyright © 2017 Alexander Shoshiashvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
